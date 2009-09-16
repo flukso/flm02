ab82a944fee04898ecdb5bf6f9c786fe13b03c93
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2009-09-16 22:39:58 +0000

    web: intial commit of taylored image_annotate module for flukso

---
+++ web/drupal/modules/image_annotate/tag.js
@@ -0,0 +1,330 @@
+Drupal.behaviors.imageAnnotate = function (context) {
+  var annotativeImage = new Array();
+  for (var i=0; i<Drupal.settings.imageAnnotate.length; i++) {
+    annotativeImage[i] = new Drupal.annotativeImage(Drupal.settings.imageAnnotate[i]);
+  }
+  // If the URL contains a fragment starting with image-annotate we define the aid of the note to highlight/show
+  var url = document.location.toString();
+  var highlight = 0;
+  if (url.match('#image-annotate-')) {
+    highlight = url.split('#image-annotate-')[1];
+    for (var i=0; i<annotativeImage.length; i++) {
+      annotativeImage[i].showNote(highlight);
+    }
+  }
+  // We replace the target of the comment links
+  $('a.image-annotate-link').click(function() {
+    for (var i=0; i<annotativeImage.length; i++) {
+      annotativeImage[i].showNote($(this).attr('rel').split('image-annotate-')[1]);
+    }
+    return false;
+  });
+};
+
+/**
+ * An annotative image object
+ */
+Drupal.annotativeImage = function (image) {
+  //BVDM 13/09/09: substitute image.field for image.nid
+  this.image = $('img.image-annotate-nid-' + image.nid);
+  this.nid = image.nid;
+  this.field = image.field;
+  this.mode = 'view';
+  // Add the canvas (which has the image as a background) and the containers for the notes
+  this.canvas = $('<div class="image-annotate-canvas"><div class="image-annotate-view"></div><div class="image-annotate-edit"><div class="image-annotate-edit-area"></div></div></div>');
+  this.canvas.children('.image-annotate-edit').hide();
+  this.canvas.children('.image-annotate-view').hide();
+  this.image.after(this.canvas);
+  // Give the canvas and the container their size and background
+  this.canvas.height(this.image.height());
+  this.canvas.width(this.image.width());
+  this.canvas.css('background-image', 'url("'+ this.image.attr('src') +'")');  
+  this.canvas.children('.image-annotate-view, .image-annotate-edit').height(this.image.height());
+  this.canvas.children('.image-annotate-view, .image-annotate-edit').width(this.image.width());
+  // Add the behavior: hide/show the notes when hovering the picture
+  this.canvas.hover(
+    function() {
+      if ($(this).children('.image-annotate-edit').css('display') == 'none') {
+        $(this).children('.image-annotate-view').show();
+      }
+    },
+    function() {
+      $(this).children('.image-annotate-view').hide();
+    }
+  );
+  this.canvas.children('.image-annotate-view').hover(
+    function() {
+      $(this).show();
+    },
+    function() {
+      $(this).hide();
+    }
+  );
+  // Create the notes
+  this.notes = new Array();
+  for (var i=0; i<image.notes.length; i++) {
+    this.notes[image.notes[i].aid] = new Drupal.imageAnnotation(this, image.notes[i]);
+  }
+  // Add the "Add a note" button
+  if (image.editable) {
+    this.button = $('<a class="image-annotate-add" id="image-annotate-add-'+ image.field +'">' + Drupal.t('Add a note') + '</a>');
+    var image = this;
+    this.button.click(function(){
+      image.addNote();
+    });
+    this.image.before(this.button);
+  }
+  else {
+    this.image.before($('<a class="image-annotate-add" id="image-annotate-add-'+ image.field +'"></a>'));
+  }
+  // Hide the original
+  this.image.hide();
+};
+
+/**
+ * Highlight and show one of the notes
+ */
+Drupal.annotativeImage.prototype.showNote = function(aid) {
+  for (key in this.notes) {
+    if (key == aid) {
+      var highlight = this.notes[key];
+    }
+    this.notes[key].hide();
+  }
+  if (highlight) {
+    this.canvas.children('.image-annotate-view').show();
+    highlight.show();
+    $('html, body').animate({scrollTop: highlight.area.offset().top}, 'slow'); // Hack with html & body scrolling so that it works in Safari
+  }
+};
+
+/**
+ * Add a note
+ */
+Drupal.annotativeImage.prototype.addNote = function () {
+  if (this.mode == 'view') {
+    this.mode = 'edit';
+    var image = this;
+    // Create/prepare the editable note elements
+    var editable = new Drupal.imageAnnotationEditable(this);
+    // Load the form and set the draggable/resizable area
+    editable.note.load(Drupal.settings.basePath + 'content/image-annotate/create/' + this.nid, {}, function() {
+      Drupal.behaviors.collapse(editable.note);
+      var form = $('#image-annotate-edit-form form');
+      // TODO: remove these *EVIL* fixes
+      form.attr('action', Drupal.settings.basePath + 'comment/reply/' + image.nid); /* Evil! */
+      $('#image-annotate-edit-form input').attr('for', ''); /* Kicking babies evil! */
+      // Add the image note information to the form action on submission
+      form.submit(function() {
+        var areaFields = $('<input type="hidden" value="1" name="image-annotate"/>'+
+          '<input type="hidden" value="'+ editable.area.height() +'" name="image-annotate-height"/>'+
+          '<input type="hidden" value="'+ editable.area.width() +'" name="image-annotate-width"/>'+
+          '<input type="hidden" value="'+ editable.area.position().top +'" name="image-annotate-top"/>'+
+          '<input type="hidden" value="'+ editable.area.position().left +'" name="image-annotate-left"/>'+
+          '<input type="hidden" value="'+ editable.image.field +'" name="image-annotate-field"/>');
+        form.append(areaFields);
+      });
+      // We add the cancel/close button
+      var cancel = $('<a class="image-annotate-edit-close">'+ Drupal.t('Cancel') +'</a>');
+      cancel.click(function() {
+        editable.destroy();
+        image.mode = 'view';
+      });
+      editable.note.prepend(cancel);
+    });
+  }
+};
+
+/**
+ * An image annotation
+ */
+Drupal.imageAnnotation = function (image, note) {
+  this.image = image;
+  this.aid = note.aid;
+  this.cid = note.cid;
+  this.height = note.height;
+  this.width = note.width;
+  this.left = note.left;
+  this.top = note.top;
+  this.editable = note.editable;
+  // Add the area
+  this.area = $('<div class="image-annotate-area'+ (this.editable ? ' image-annotate-area-editable' : '') +'"><div></div></div>');
+  this.image.canvas.children('.image-annotate-view').prepend(this.area);
+  // Add the note
+  this.note = $('<div class="image-annotate-note">'+ note.text +'</div>');
+  this.note.hide();
+  this.image.canvas.children('.image-annotate-view').append(this.note);
+  this.note.children('span.actions').hide();
+  // Set the position and size of the note
+  this.set();
+  // Add the behavior: hide/display the note when hovering the area
+  var annotation = this;
+  this.area.hover(
+    function() {
+      annotation.show();
+    },
+    function() {
+      annotation.hide();
+    }
+  );
+  this.note.hover(
+    function(){
+      annotation.show();
+      annotation.note.children('span.actions').show('slow');
+    },
+    function(){
+      annotation.hide();
+      annotation.note.children('span.actions').hide();
+    }
+  );
+  // Edit a note feature
+  if (this.editable) {
+    var note = this;
+    this.area.click(function () {
+      note.edit();
+    });
+  }
+};
+
+/**
+ * Set the position and size of the note
+ */
+Drupal.imageAnnotation.prototype.set = function() {
+  this.area.children('div').height((this.height - 2) +'px');
+  this.area.children('div').width((this.width - 2) +'px');
+  this.area.css('left', (this.left) +'px');
+  this.area.css('top', (this.top) +'px');
+  this.note.css('left', (this.left) +'px');
+  this.note.css('top', (parseInt(this.top) + parseInt(this.height) + 2) +'px');
+};
+
+/**
+ * Highlight/show the note
+ */
+Drupal.imageAnnotation.prototype.show = function() {
+  this.note.show();
+  this.area.addClass('image-annotate-area-hover');
+};
+
+/**
+ * Hide the note
+ */
+Drupal.imageAnnotation.prototype.hide = function() {
+  this.note.hide();
+  this.area.removeClass('image-annotate-area-hover');
+};
+
+/**
+ * Show the note edit form
+ */
+Drupal.imageAnnotation.prototype.edit = function() {
+  if (this.image.mode == 'view') {
+    this.image.mode = 'edit';
+    var note = this;
+    // Create/prepare the editable note elements
+    var editable = new Drupal.imageAnnotationEditable(this.image, this);
+    // Load the form and set the draggable/resizable area
+    editable.note.load(Drupal.settings.basePath + 'content/image-annotate/edit/' + this.aid, {}, function() {
+      Drupal.behaviors.collapse(editable.note);
+      var form = $('#image-annotate-edit-form form');
+      // TODO: remove these *EVIL* fixes
+      form.attr('action', Drupal.settings.basePath + 'comment/edit/'+ note.cid); /* Evil! */
+      $('#image-annotate-edit-form input').attr('for', ''); /* Kicking babies evil! */
+      // Add the image note information to the form action on submission
+      form.submit(function() {
+        var areaFields = $('<input type="hidden" value="1" name="image-annotate"/>'+
+          '<input type="hidden" value="'+ editable.area.height() +'" name="image-annotate-height"/>'+
+          '<input type="hidden" value="'+ editable.area.width() +'" name="image-annotate-width"/>'+
+          '<input type="hidden" value="'+ editable.area.position().top +'" name="image-annotate-top"/>'+
+          '<input type="hidden" value="'+ editable.area.position().left +'" name="image-annotate-left"/>'+
+          '<input type="hidden" value="'+ editable.image.field +'" name="image-annotate-field"/>');
+        form.append(areaFields);
+        // var areaInfo = editable.area.position().top +'/'+ editable.area.position().left +'/'+ editable.area.height() +'/'+ editable.area.width();
+        // 	  form.attr('action', form.attr('action') +'/image-annotate/'+ areaInfo);
+      });
+      // We add the cancel/close button
+      var cancel = $('<a class="image-annotate-edit-close">'+ Drupal.t('Cancel') +'</a>');
+      cancel.click(function() {
+        editable.destroy();
+        note.image.mode = 'view';
+      });
+      editable.note.prepend(cancel);
+    });
+  }
+};
+
+/**
+ * The annotation form
+ */
+Drupal.imageAnnotationEditable = function (image, note) {
+  this.image = image;
+  // Set up the area
+  this.area = this.image.canvas.children('.image-annotate-edit').children('.image-annotate-edit-area');
+  if (note) {
+    this.area.css('height', note.height +'px');
+    this.area.css('width', note.width +'px');
+    this.area.css('left', note.left +'px');
+    this.area.css('top', note.top +'px');
+  }
+  // Show the edition canvas and hide the view canvas
+  this.image.canvas.children('.image-annotate-view').hide();
+  this.image.canvas.children('.image-annotate-edit').show();
+  // Add the note (which we'll load with the form afterwards)
+  this.note = $('<div id="image-annotate-edit-form"></div>');
+  $('body').append(this.note);
+  this.note.css('left', this.area.offset().left +'px');
+  this.note.css('top', (parseInt(this.area.offset().top) + parseInt(this.area.height()) + 2) +'px');
+  // Set the area as a draggable/resizable element contained in the image canvas.
+  // Would be better to use the containment option for resizable but buggy
+  var editable = this;
+  var area = this.area;
+  var note = this.note;
+  this.area.resizable({
+    handles: 'all',
+	  resize: function(e, ui) {
+      if (parseInt(area.position().top) + parseInt(area.height()) + 2 > parseInt(editable.image.canvas.height())) {
+        area.height(parseInt(editable.image.canvas.height()) - parseInt(area.position().top) - 2);
+      }
+      if (parseInt(area.position().left) + parseInt(area.width()) + 2 > parseInt(editable.image.canvas.width())) {
+        area.width(parseInt(editable.image.canvas.width()) - parseInt(area.position().left) - 2);
+      }
+      if (parseInt(area.position().top) < 0) {
+        area.height(parseInt(editable.image.canvas.height())).css('top', 0);
+      }
+      if (parseInt(area.position().left) < 0) {
+        area.width(parseInt(editable.image.canvas.width())).css('left', 0);
+      }
+      note.css('left', area.offset().left +'px');
+	  note.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 2) +'px');
+    },
+    stop: function(e, ui) {
+      note.css('left', area.offset().left +'px');
+	  note.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 2) +'px');
+    }
+  })
+  .draggable({
+    containment: editable.image.canvas,
+    drag: function(e, ui) {
+      note.css('left', area.offset().left +'px');
+	  note.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 2) +'px');
+	},
+    stop: function(e, ui) {
+      note.css('left', area.offset().left +'px');
+	  note.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 2) +'px');
+    }
+  });
+};
+
+/**
+ * Destroy the annotation form
+ */
+Drupal.imageAnnotationEditable.prototype.destroy = function () {
+  this.image.canvas.children('.image-annotate-edit').hide();
+  this.area.resizable('destroy');
+  this.area.draggable('destroy');
+  this.area.css('height', '');
+  this.area.css('width', '');
+  this.area.css('left', '');
+  this.area.css('top', '');
+  this.note.remove();
+};
--- web/drupal/themes/flukso/template.php
+++ web/drupal/themes/flukso/template.php
@@ -101,3 +101,180 @@ function flukso_preprocess_page(&$vars) {
  // -- tab text sould always be Flukso
  $vars['head_title'] = 'Flukso';
 }
+
+/**
+  * Support for image_annotate on image nodes
+  *
+  */
+function phptemplate_image_body($node, $size) {
+  if (user_access('view image annotations') || user_access('create image annotations') || user_access('administer image annotations')) {
+    // Retrieve all the annotations for that image field
+    // We sort by area (height*width) to make sure small annotations are always on the top and avoid having some unhoverable ones
+    $result = db_query('SELECT i.*, c.uid, c.comment, u.name FROM {image_annotate} i INNER JOIN {comments} c ON i.cid = c.cid JOIN {users} u ON c.uid = u.uid WHERE c.nid = %d ORDER BY (i.size_height*i.size_width) ASC', $node->nid);
+
+    // Build the array of notes settings
+    global $user;
+    $notes = array();
+    while ($note = db_fetch_object($result)) {
+      $editable = user_access('administer image annotations') || (user_access('create image annotations') && $note->uid && $note->uid == $user->uid);
+      $author = theme('username', $note);
+      $text = check_plain($note->comment); // . '"<span class="author"> '. t('by') .' '. $author . '</span>';
+
+//      if (user_access('access comments')) {
+//        $text .= '<span class="actions"> » '. l(t('View comment'), $_GET['q'], array('fragment'=>'comment-'. $note->cid)) .'</span>';
+//      }
+
+      $notes[] = array(
+        'aid' => $note->aid,
+        'cid' => $note->cid,
+        'uid' => $note->uid,
+        'height' => $note->size_height,
+        'width' => $note->size_width,
+        'top' => $note->position_top,
+        'left' => $note->position_left,
+        'text' => $text,
+        'editable' => $editable,
+      );
+    }
+   
+    // Build the field settings
+    $settings = array(array(
+      'nid' => $node->nid,
+      'field' => 'image',
+      'notes' => $notes,
+      'editable' => user_access('administer image annotations') || user_access('create image annotations'),
+    ));
+   
+    // Load all the JS and CSS magic
+    drupal_add_js(array('imageAnnotate' => $settings), 'setting');
+    jquery_ui_add(array('ui.resizable', 'ui.draggable'));
+    drupal_add_js('misc/collapse.js');
+    drupal_add_js(drupal_get_path('module', 'image_annotate') .'/tag.js');
+    drupal_add_css(drupal_get_path('module', 'image_annotate') .'/tag.css');
+    //BVDM 13/09/09: substitute image-annotate-image for image-annotate-nid-$node->nid to create a unique class per inserted image
+    $class = 'imagefield imagefield-image image-annotate-nid-' . $node->nid;
+    return image_display($node, $size, array('class' => $class));
+  }
+}
+
+/**
+  * Support for image_annotate on img_assist inserted images
+  *
+  */
+function phptemplate_img_assist_inline($node, $size, $attributes) {
+  $caption = '';
+  if ($attributes['title'] && $attributes['desc']) {
+    $caption = '<strong>'. $attributes['title'] .': </strong>'. $attributes['desc'];
+  }
+  elseif ($attributes['title']) {
+    $caption = '<strong>'. $attributes['title'] .'</strong>';
+  }
+  elseif ($attributes['desc']) {
+    $caption = $attributes['desc'];
+  }
+  // Change the node title because img_assist_display() uses the node title for
+  // alt and title.
+  $node->title = strip_tags($caption);
+
+  // --------------------------
+
+  if (user_access('view image annotations') || user_access('create image annotations') || user_access('administer image annotations')) {
+    // Retrieve all the annotations for that image field
+    // We sort by area (height*width) to make sure small annotations are always on the top and avoid having some unhoverable ones
+    $result = db_query('SELECT i.*, c.uid, c.comment, u.name FROM {image_annotate} i INNER JOIN {comments} c ON i.cid = c.cid JOIN {users} u ON c.uid = u.uid WHERE c.nid = %d ORDER BY (i.size_height*i.size_width) ASC', $node->nid);
+
+    // Build the array of notes settings
+    global $user;
+    $notes = array();
+    while ($note = db_fetch_object($result)) {
+      $editable = user_access('administer image annotations') || (user_access('create image annotations') && $note->uid && $note->uid == $user->uid);
+      $author = theme('username', $note);
+      $text = check_plain($note->comment); // . '"<span class="author"> '. t('by') .' '. $author . '</span>';
+
+//      if (user_access('access comments')) {
+//        $text .= '<span class="actions"> » '. l(t('View comment'), $_GET['q'], array('fragment'=>'comment-'. $note->cid)) .'</span>';
+//      }
+
+      $notes[] = array(
+        'aid' => $note->aid,
+        'cid' => $note->cid,
+        'uid' => $note->uid,
+        'height' => $note->size_height,
+        'width' => $note->size_width,
+        'top' => $note->position_top,
+        'left' => $note->position_left,
+        'text' => $text,
+        'editable' => $editable,
+      );
+    }
+   
+    // Build the field settings
+    $settings = array(array(
+      'nid' => $node->nid,
+      'field' => 'image',
+      'notes' => $notes,
+      'editable' => user_access('administer image annotations') || user_access('create image annotations'),
+    ));
+   
+    // Load all the JS and CSS magic
+    drupal_add_js(array('imageAnnotate' => $settings), 'setting');
+    jquery_ui_add(array('ui.resizable', 'ui.draggable'));
+    drupal_add_js('misc/collapse.js');
+    drupal_add_js(drupal_get_path('module', 'image_annotate') .'/tag.js');
+    drupal_add_css(drupal_get_path('module', 'image_annotate') .'/tag.css');
+    //BVDM 13/09/09: substitute image-annotate-image for image-annotate-nid-$node->nid to create a unique class per inserted image
+    $class = 'imagefield imagefield-image image-annotate-nid-' . $node->nid;
+    $img_tag = img_assist_display($node, $size, array('class' => $class));
+  }
+  else {
+    $img_tag = img_assist_display($node, $size);
+  }
+
+  // -----------------------
+
+  // Always define an alignment class, even if it is 'none'.
+  $output = '<span class="inline inline-'. $attributes['align'] .'">';
+
+  $link = $attributes['link'];
+  $url  = '';
+  // Backwards compatibility: Also parse link/url in the format link=url,foo.
+  if (strpos($link, ',') !== FALSE) {
+    list($link, $url) = explode(',', $link, 2);
+  }
+  elseif (isset($attributes['url'])) {
+    $url = $attributes['url'];
+  }
+  
+  if ($link == 'node') {
+    $output .= l($img_tag, 'node/'. $node->nid, array('html' => TRUE));
+  }
+  elseif ($link == 'popup') {
+    $popup_size = variable_get('img_assist_popup_label', IMAGE_PREVIEW);
+    $info       = image_get_info(file_create_path($node->images[$popup_size]));
+    $width      = $info['width'];
+    $height     = $info['height'];
+    $popup_url  = file_create_url($node->images[variable_get('img_assist_popup_label', IMAGE_PREVIEW)]);
+    $output .= l($img_tag, $popup_url, array('attributes' => array('onclick' => "launch_popup({$node->nid}, {$width}, {$height}); return false;", 'target' => '_blank'), 'html' =>TRUE));
+  }
+  elseif ($link == 'url') {
+    $output .= l($img_tag, $url, array('html' => TRUE));
+  }
+  else {
+    $output .= $img_tag;
+  }
+  
+  if ($caption) {
+    if ($attributes['align'] != 'center') {
+      $info = image_get_info(file_create_path($node->images[$size['key']]));
+      // Reduce the caption width slightly so the variable width of the text
+      // doesn't ever exceed image width.
+      $width = $info['width'] - 2;
+      $output .= '<span class="caption" style="width: '. $width .'px;">'. $caption .'</span>';
+    }
+    else {
+      $output .= '<span class="caption">'. $caption .'</span>';
+    }
+  }
+  $output .= '</span>';
+  return $output;
+}
