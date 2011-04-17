7b81c079f6162a9ccb998b8a1ce38b6e24a16a6e
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2011-04-17 22:08:56 +0200

    [fluksod] add a crontab entry that checks up on the daemon every 15min

---
+++ mote/v2/openwrt/package/flukso/config/fcheck.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+#  fcheck - Check whether the Flukso daemon components are still running.
+#
+#  Copyright (C) 2011 Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+[ -z "$(ps | grep 'spi[d]')" ] && exit 1
+[ -z "$(ps | grep 'flukso[d]')" ] && exit 2
+
+exit 0
