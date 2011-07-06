10b576cb71907b917c3ff3792fe0c8b008fa8144
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2011-07-06 22:16:25 +0200

    [avr] trigger a pulse for an analog port when >= UNIT

--- avr/main.c
+++ avr/main.c
@@ -206,7 +206,7 @@ ISR(TIMER1_COMPA_vect)
 
 		MacU16X16to32(state[sensor_id].nano, sensor[sensor_id].meterconst, ADC);
 
-		if (state[sensor_id].nano > UNIT) {
+		if (state[sensor_id].nano >= UNIT) {
 			sensor[sensor_id].counter++;
 
 			state[sensor_id].flags |= STATE_PULSE;
