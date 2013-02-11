cac9c819c1ae133103faeb9d58ee5e5ce3b8360e
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2013-02-11 21:41:09 +0100

    [fluksod] fix whitespace

--- openwrt/package/flukso/luasrc/fluksod.lua
+++ openwrt/package/flukso/luasrc/fluksod.lua
@@ -30,43 +30,43 @@ local httpclient = require 'luci.httpclient'
 local data       = require 'flukso.data'
 
 -- parse and load /etc/config/flukso
-local FLUKSO		= uci:get_all('flukso')
+local FLUKSO            = uci:get_all('flukso')
 
 local arg = arg or {} -- needed when this code is not loaded via the interpreter
 
-local DEBUG		= (arg[1] == '-d')
-local LOGMASK		= FLUKSO.daemon.logmask or 'info'
+local DEBUG             = (arg[1] == '-d')
+local LOGMASK           = FLUKSO.daemon.logmask or 'info'
 nixio.setlogmask(LOGMASK)
 
-local DAEMON 		= os.getenv('DAEMON') or 'fluksod'
-local DAEMON_PATH 	= os.getenv('DAEMON_PATH') or '/var/run/' .. DAEMON
+local DAEMON 		    = os.getenv('DAEMON') or 'fluksod'
+local DAEMON_PATH 	    = os.getenv('DAEMON_PATH') or '/var/run/' .. DAEMON
 
-local DELTA_PATH	= '/var/run/spid/delta'
-local DELTA_PATH_IN	= DELTA_PATH .. '/in'
-local DELTA_PATH_OUT	= DELTA_PATH .. '/out'
+local DELTA_PATH        = '/var/run/spid/delta'
+local DELTA_PATH_IN	    = DELTA_PATH .. '/in'
+local DELTA_PATH_OUT    = DELTA_PATH .. '/out'
 
-local O_RDWR		= nixio.open_flags('rdwr')
+local O_RDWR		    = nixio.open_flags('rdwr')
 local O_RDWR_NONBLOCK   = nixio.open_flags('rdwr', 'nonblock')
-local O_RDWR_CREAT	= nixio.open_flags('rdwr', 'creat')
+local O_RDWR_CREAT	    = nixio.open_flags('rdwr', 'creat')
 
 local POLLIN            = nixio.poll_flags('in')
 
 -- set WAN parameters
-local WAN_ENABLED	= (FLUKSO.daemon.enable_wan_branch == '1')
+local WAN_ENABLED       = (FLUKSO.daemon.enable_wan_branch == '1')
 
-local TIMESTAMP_MIN	= 1234567890
-local WAN_INTERVAL	= 300
+local TIMESTAMP_MIN	    = 1234567890
+local WAN_INTERVAL	    = 300
 
-local WAN_FILTER = { [1] = {}, [2] = {}, [3] = {} }
-WAN_FILTER[1].span	= 60
-WAN_FILTER[1].offset	= 0
-WAN_FILTER[2].span	= 900
-WAN_FILTER[2].offset	= 7200
-WAN_FILTER[3].span	= 86400
-WAN_FILTER[3].offset	= 172800
+local WAN_FILTER        = { [1] = {}, [2] = {}, [3] = {} }
+WAN_FILTER[1].span      = 60
+WAN_FILTER[1].offset    = 0
+WAN_FILTER[2].span      = 900
+WAN_FILTER[2].offset    = 7200
+WAN_FILTER[3].span      = 86400
+WAN_FILTER[3].offset    = 172800
 
-local WAN_BASE_URL	= FLUKSO.daemon.wan_base_url .. 'sensor/'
-local WAN_KEY		= '0123456789abcdef0123456789abcdef'
+local WAN_BASE_URL      = FLUKSO.daemon.wan_base_url .. 'sensor/'
+local WAN_KEY           = '0123456789abcdef0123456789abcdef'
 uci:foreach('system', 'system', function(x) WAN_KEY = x.key end) -- quirky but it works
 
 -- https headers
@@ -74,20 +74,22 @@ local FLUKSO_VERSION    = '000'
 uci:foreach('system', 'system', function(x) FLUKSO_VERSION = x.version end) -- quirky but it works, again
 
 local USER_AGENT        = 'Fluksometer v' .. FLUKSO_VERSION
-local CACERT		= FLUKSO.daemon.cacert
+local CACERT            = FLUKSO.daemon.cacert
 
 -- set LAN parameters
-local LAN_ENABLED	= (FLUKSO.daemon.enable_lan_branch == '1')
+local LAN_ENABLED       = (FLUKSO.daemon.enable_lan_branch == '1')
 
-local LAN_INTERVAL	= 0
+local LAN_INTERVAL      = 0
 local LAN_POLISH_CUTOFF	= 60
-local LAN_PUBLISH_PATH	= DAEMON_PATH .. '/sensor'
+local LAN_PUBLISH_PATH  = DAEMON_PATH .. '/sensor'
 
-local LAN_FACTOR	= { ['electricity'] =      3.6e6,	-- 1 Wh/ms = 3.6e6 W
-			    ['water']       = 24 * 3.6e6,	-- 1 L/ms  = 24 * 3.6e6 L/day
-			    ['gas']         = 24 * 3.6e6 }	-- 1 L/ms  = 24 * 3.6e6 L/day
+local LAN_FACTOR = {
+	['electricity']     =      3.6e6, -- 1 Wh/ms = 3.6e6 W
+	['water']           = 24 * 3.6e6, -- 1 L/ms  = 24 * 3.6e6 L/day
+	['gas']             = 24 * 3.6e6  -- 1 L/ms  = 24 * 3.6e6 L/day
+}
 
-local LAN_ID_TO_FACTOR	= { }
+local LAN_ID_TO_FACTOR  = { }
 uci:foreach('flukso', 'sensor', function(x) LAN_ID_TO_FACTOR[x.id] = LAN_FACTOR[x['type']] end)
 
 function resume(...)
@@ -100,8 +102,10 @@ end
 
 function dispatch(wan_child, lan_child)
 	return coroutine.create(function()
-		local delta = { fdin  = nixio.open(DELTA_PATH_IN, O_RDWR_NONBLOCK),
-                                fdout = nixio.open(DELTA_PATH_OUT, O_RDWR) }
+		local delta = {
+			fdin  = nixio.open(DELTA_PATH_IN, O_RDWR_NONBLOCK),
+			fdout = nixio.open(DELTA_PATH_OUT, O_RDWR)
+		}
 
 		if delta.fdin == nil or delta.fdout == nil then
 			nixio.syslog('alert', 'cannot open the delta fifos')
