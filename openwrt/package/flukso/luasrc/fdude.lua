fd63ccf26168dc9027a636d329bb5535212d9be9
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2013-03-03 15:50:28 +0100

    [fdude] include support for FLM02A/B variants

--- openwrt/package/flukso/luasrc/fdude.lua
+++ openwrt/package/flukso/luasrc/fdude.lua
@@ -23,16 +23,47 @@
 
 
 local nixio = require 'nixio'
+local uci   = require 'luci.model.uci'.cursor()
 
-local SPI_DEV			= '/dev/spidev0.0'
-local SPI_DAEMON_PID_FILE	= '/var/run/spid/pid'
-local O_RDWR_NONBLOCK		= nixio.open_flags('rdwr', 'nonblock')
+local SPI_DEV             = '/dev/spidev0.0'
+local SPI_DAEMON_PID_FILE = '/var/run/spid/pid'
+local O_RDWR_NONBLOCK     = nixio.open_flags('rdwr', 'nonblock')
+local MODEL               = 'FLM02X'
+uci:foreach('system', 'system', function(x) MODEL = x.model end)
+local AVR_DIR        = arg[1] or '/usr/bin/avr'
 
 local spidev = nixio.open(SPI_DEV, O_RDWR_NONBLOCK)
 local exit = 0
 
+local function avrdude_cmd()
+	local opt = {'', '-p atmega168p'}
+
+	if MODEL == 'FLM02A' then
+		opt[#opt+1] = '-c flm02a'
+		opt[#opt+1] = '-U lfuse:w:0xEC:m'
+		opt[#opt+1] = '-U hfuse:w:0xDE:m'
+		opt[#opt+1] = '-U efuse:w:0x01:m'
+		opt[#opt+1] = '-U flash:w:' .. AVR_DIR .. '/a/main.hex'
+		opt[#opt+1] = '-U eeprom:w:' .. AVR_DIR .. '/a/main.eep'
+		
+	elseif MODEL == 'FLM02B' then
+		opt[#opt+1] = '-c flm02b'
+		opt[#opt+1] = '-U lfuse:w:0x6E:m'
+		opt[#opt+1] = '-U hfuse:w:0xDE:m'
+		opt[#opt+1] = '-U efuse:w:0x01:m'
+		opt[#opt+1] = '-U flash:w:' .. AVR_DIR .. '/b/main.hex'
+		opt[#opt+1] = '-U eeprom:w:' .. AVR_DIR .. '/b/main.eep'
+	else
+		print('Unrecognised Fluksometer model')
+		exit = 3
+		return ''
+	end
+
+	return '/usr/bin/avrdude' .. table.concat(opt, ' ')
+end
+
 if spidev:lock('tlock') then
-	nixio.execp('avrdude', ...)
+	nixio.execp(avrdude_cmd())
 else
 	print(string.format('Detected a lock on %s', SPI_DEV))
 
