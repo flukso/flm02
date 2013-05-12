59a7832854c8017591ba91dc80f60ce85e4c9e55
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2013-05-12 10:22:57 +0200

    [uhttpd] kill cgi process after time-out to prevent uhttpd lock-up

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -55,6 +55,10 @@ rm $BACKFIRE_PATH/package/lua/patches/400-luaposix_5.1.4-embedded.patch
 rm $BACKFIRE_PATH/package/lua/patches/500-eglibc_config.patch
 cp patches/600-lua-tablecreate.patch $BACKFIRE_PATH/package/lua/patches
 
+# patch the uhttpd package
+mkdir $BACKFIRE_PATH/package/uhttpd/patches
+cp patches/610-kill_cgi_process_after_timeout.patch $BACKFIRE_PATH/package/uhttpd/patches
+
 # copy flash utility to the tools dir
 cp ../tools/ap51-flash $BACKFIRE_PATH/tools
 
